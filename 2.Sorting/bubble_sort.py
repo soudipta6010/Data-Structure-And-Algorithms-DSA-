4fe47a32d9bae35749bc47aa380e65f207e063f0
Soudipta Sarkar <soudiptass26@gmail.com>
2025-03-24 20:11:59 +0530

    initial commit

--- 2.Sorting/bubble_sort.py
+++ 2.Sorting/bubble_sort.py
@@ -1,25 +1,26 @@
-nums = [4, 8, 7, 1, 3, 9, 10, 5, 6, 15]
+nums = [4, 8, 7, 1, 3, 9, 10, 5, 6, 15, 2]
 
 
 # Worst case / Average case solution(TC-> O(n^2), SC -> O(1))
 def bubble_sort(nums):
     n = len(nums)
-    for i in range(n - 2, 1, -1):
-        for j in range(0, i):
+    for i in range(n - 2, 0, -1):
+        for j in range(0, i + 1):
             if nums[j] > nums[j + 1]:
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
     return nums
 
 
+print(f"Before sorting: {nums}")
 print(bubble_sort(nums))
 
 
 # Best case solution(TC-> O(n), SC -> O(1))
 def bubble_sort_2(nums):
     n = len(nums)
-    for i in range(n - 2, 1, -1):
+    for i in range(n - 2, 0, -1):
         is_swap = False
-        for j in range(0, i):
+        for j in range(0, i + 1):
             if nums[j] > nums[j + 1]:
                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
                 is_swap = True
@@ -28,4 +29,5 @@ def bubble_sort_2(nums):
     return nums
 
 
+print(f"Before sorting: {nums}")
 print(bubble_sort_2(nums))
