b50266c3fe416552971b511677074470746ab3ab
Soudipta Sarkar <soudiptass26@gmail.com>
2026-04-09 23:59:32 +0530

    doubly linkedlist

---
+++ 4.Linked_List/Doubly_linked_list/doubly_linked_list.py
@@ -0,0 +1,50 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert At head [TC -> O(1), SC-> O(1)]
+    def insert_at_head(self, val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node  # First node becomes head
+        else:
+            new_node.next = self.head  # New node points to current head
+            self.head.prev = new_node  # Current head's prev points to new node
+            self.head = new_node  # Update head to new node
+
+    #  Insert at End (Append), [TC -> O(N), SC-> O(1)]
+    def append(self, val):
+        new_node = Node(val)
+        if not self.head:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next      # Find the last node
+            current.next = new_node         # Connect last node to new node    
+            new_node.prev = current         # Connect new node back to last node
+
+    # Insert at any Position of doubly linked List
+    def insert_at(self, val, position):
+        new_node = Node(val)
+        if position == 0:
+            self.insert_at_head(val)
+            return
+        
+        current = self.head
+        count = 0
+        while current and count < position - 1:
+            current = current.next
+            count += 1
+        new_node.next = current.next
+        new_node.prev = current
+        if current.next:
+            current.next.prev = new_node
+        current.next = new_node
